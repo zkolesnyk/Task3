1b8f6bd3eab58c810ba8f1b0554ce8572fd41e06
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-05 16:18:33 +0200

    upd

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ public class Main {
     public static void main(String[] args) {
         int x = 15;
         int y = 4;
-        System.out.println(prog(x, y));
+        System.out.println(resultToStr(x, y));
     }
 
-    private static String prog(int x, int y) {
+    public static float prog(int x, int y) {
         if (x % 2 == 0) {
-            return resultToStr(((float) x) * y);
+            return ((float) x) * y;
         } else if (x % 2 != 0 && y != 0) {
-            return resultToStr(((float) x) / y);
-        } else return null;
+            return ((float) x) / y;
+        } else return 0;
     }
 
-    private static String resultToStr(float z) {
-        if (Math.abs(z) == 0.00) {
-            return String.format("%.0f", z);
-        } else return String.format("%.2f", z);
+    private static String resultToStr(int x, int y) {
+        if (Math.abs(prog(x, y)) == 0.00) {
+            return String.format("%.0f", prog(x, y));
+        } else return String.format("%.2f", prog(x, y));
     }
 }

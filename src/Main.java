cf11af1b4f7eec17c0b2c12e80d6fc6e9d410c27
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-05 16:05:18 +0200

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 11;
-        int sum = 0;
+        int x = 15;
+        int y = 4;
+        System.out.println(prog(x, y));
+    }
 
-        while (n != 0) {
-            sum += n;
-            n--;
-        }
+    private static String prog(int x, int y) {
+        if (x % 2 == 0) {
+            return resultToStr(((float) x) * y);
+        } else if (x % 2 != 0 && y != 0) {
+            return resultToStr(((float) x) / y);
+        } else return null;
+    }
 
-        System.out.printf("%nsum= %d%n", sum);
+    private static String resultToStr(float z) {
+        if (Math.abs(z) == 0.00) {
+            return String.format("%.0f", z);
+        } else return String.format("%.2f", z);
     }
 }

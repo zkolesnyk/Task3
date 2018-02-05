d887898a67946596be64a6eb706362269da56bbc
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-05 16:32:22 +0200

    created test

---
+++ tests/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void progTest(int x, int y) {
+        // arrange
+        x = 1;
+        y = 100;
+        float actual = 0.01f;
+
+        // act
+        float expected = Main.prog(x, y);
+
+        // assert
+
+        assertEquals(actual, expected);
+    }
+}

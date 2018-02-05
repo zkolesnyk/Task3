d9216cb2a6400d019a86768ada4f1020296fbf91
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-05 17:53:47 +0200

    test upd

--- tests/MainTest.java
+++ tests/MainTest.java
@@ -5,16 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
+    void resultToStrEvenXTest() {
+        assertEquals(Main.resultToStr(16, 2), "32");
+        assertEquals(Main.resultToStr(14, 1), "14");
+        assertEquals(Main.resultToStr(12, 0), "0");
+        assertEquals(Main.resultToStr(10, -1), "-10");
+    }
+
+    @Test
     void resultToStrTest() {
-        // arrange
-        int x = 1;
-        int y = 1000;
-        String actual = "0,001";
-
-        // act
-        String expected = Main.resultToStr(x, y);
-
-        // assert
-        assertEquals(expected, actual);
+        assertEquals(Main.resultToStr(1, 1000), "0,001");
     }
 }

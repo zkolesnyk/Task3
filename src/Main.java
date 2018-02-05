893a305a59050290c10b6878b46309b59480b3cf
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-05 17:56:11 +0200

    test upd

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 15;
+        int x = 16;
         int y = 4;
         System.out.println(resultToStr(x, y));
     }
 
     public static float prog(int x, int y) {
         if (x % 2 == 0) {
-            return ((float) x) * y;
+            return  x * y;
         } else if (x % 2 != 0 && y != 0) {
             return ((float) x) / y;
         } else return 0;
     }
 
     public static String resultToStr(int x, int y) {
-        if (Math.abs(prog(x, y)) == 0f) {
-            return String.format("%.0f", prog(x, y));
-        } else return String.format("%.2f", prog(x, y));
+        return String.format("%f", prog(x, y));
     }
 }

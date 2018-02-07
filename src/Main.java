4052ce22fc2a2928213ad0c8fea4d9893eb6daee
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-02-07 13:53:17 +0200

    tuple

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.javatuples.Triplet;
+
 public class Main {
     public static void main(String[] args) {
         int x = 16;
@@ -5,15 +7,22 @@ public class Main {
         System.out.println(resultToStr(x, y));
     }
 
-    public static float prog(int x, int y) {
+    public static Triplet<Integer, Float, Boolean> prog(int x, int y) {
+
+        int resX = 0;
+        float resY = 0f;
         if (x % 2 == 0) {
-            return  x * y;
+            resX = x * y;
+            return new Triplet<Integer, Float, Boolean>(resX, 0f, true);
         } else if (x % 2 != 0 && y != 0) {
-            return ((float) x) / y;
-        } else return 0;
+            resY = ((float) x) / y;
+            return new Triplet<Integer, Float, Boolean>(0, resY, false);
+        }
+        return new Triplet<Integer, Float, Boolean>(0, 0f, true);
     }
 
     public static String resultToStr(int x, int y) {
-        return String.format("%f", prog(x, y));
+
+        return (prog(x, y).getValue2()) ? String.format("%s", prog(x, y).getValue0()) : String.format("%s", prog(x, y).getValue1());
     }
 }
